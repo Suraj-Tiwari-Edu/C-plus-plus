f21050564876e0176c2f72def9513c9bb669eebe
Your Name <your.email@example.com>
2025-02-01 23:38:59 +0530

    initial commit

---
+++ Sum_of_two_num.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+typedef int num;
+int main()
+{ 
+    num a,b,c;
+    cout<<"enter a and b"<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"addition is "<<c;
+    return 0;
+}
